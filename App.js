c9bf7a60753fe6732650e89c663fe1901b313f15
Fellyp Santos <fellypsantos2011@gmail.com>
2021-07-26 01:24:17 -0300

    Project started.

--- App.js
+++ App.js
@@ -1,37 +1,112 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import TelaInicio from './src/telas/Inicio';
-import Configuracoes from './src/telas/Configuracoes';
-import Home from './src/telas/Home';
-import MeusRegistros from './src/telas/MeusRegistros';
-import NovoRegistro from './src/telas/NovoRegistro';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-const AppNavigator = createStackNavigator({
-  TelaInicio: {screen: TelaInicio},
-  Home: {screen: Home},
-  Configuracoes: {
-    screen: Configuracoes,
-    navigationOptions: {
-      title: 'Configurações'
-    }
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
   },
-  MeusRegistros: {
-    screen: MeusRegistros,
-    navigationOptions: {
-      title: 'Meus alimentos'
-    }
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
   },
-  NovoRegistro: {
-    screen: NovoRegistro,
-    navigationOptions: ({ navigation }) => ({
-      title: (navigation.getParam('itemEditar') != undefined) ? 'Editar registro' : 'Novo registro alimentar',
-    })
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
   },
-}, {
-  initialRouteName: 'TelaInicio',
-  defaultNavigationOptions: {
-    headerStyle: { backgroundColor: '#7F22A7' },
-    headerTintColor: '#fff',
-  }
+  highlight: {
+    fontWeight: '700',
+  },
 });
 
-export default createAppContainer(AppNavigator);
+export default App;
