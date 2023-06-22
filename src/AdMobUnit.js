b525a6486acb06b56e79db187311c9dc050a2b8b
fellypsantos <fellypsantos2011@gmail.com>
2023-06-22 14:58:32 -0300

    updated ad units

--- src/AdMobUnit.js
+++ src/AdMobUnit.js
@@ -1,12 +1,28 @@
+const TestAd = {
+  Banner: 'ca-app-pub-3940256099942544/6300978111',
+  AppOpen: 'ca-app-pub-3940256099942544/3419835294',
+  Interstitial: 'ca-app-pub-3940256099942544/1033173712',
+  Rewarded: 'ca-app-pub-3940256099942544/5224354917',
+};
+
+const footer = !__DEV__
+  ? 'ca-app-pub-3444194669126701/6340575589'
+  : TestAd.Banner;
+
+const useless = !__DEV__
+  ? 'ca-app-pub-3444194669126701/6296381544'
+  : TestAd.Banner;
+
+const disableAdsTemporary = !__DEV__
+  ? 'ca-app-pub-3444194669126701/2712359756'
+  : TestAd.Rewarded;
+
+
 export default {
   Banner: {
-    Footer: 'ca-app-pub-3444194669126701/2896287464',
-  },
-  Interstitial: {
-    Premium: 'ca-app-pub-3444194669126701/6444212865',
+    Footer: footer,
   },
   Rewarded: {
-    Premium: 'ca-app-pub-3444194669126701/9826012534',
-    ProjectSponsor: 'ca-app-pub-3444194669126701/5608838417',
+    Premium: disableAdsTemporary,
   },
 };
