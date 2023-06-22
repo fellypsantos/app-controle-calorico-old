a6ab85939744770eaa1de09cb3d77cea55baef49
fellypsantos <fellypsantos2011@gmail.com>
2023-06-22 14:57:55 -0300

    nav icon

--- src/components/AdIconsBar/index.js
+++ src/components/AdIconsBar/index.js
@@ -1,29 +1,15 @@
-import React, {useState, useEffect, useContext} from 'react';
-import {TouchableOpacity, ActivityIndicator, Alert} from 'react-native';
-import {AdMobInterstitial} from 'react-native-admob';
-import {useNavigation} from '@react-navigation/native';
+import React, { useContext } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import {AdButtonContainer, AdButtonBlock} from './styles';
+import { AdButtonContainer, AdButtonBlock } from './styles';
 
-import {ProfileContext} from '../../Contexts/ProfileContext';
+import { ProfileContext } from '../../Contexts/ProfileContext';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import Toaster from '../../Toaster';
-import AdMobUnit from '../../AdMobUnit';
 
 const AdIconsBar = () => {
   const navigation = useNavigation();
-  const {isPremiumTime, Translator} = useContext(ProfileContext);
-
-  useEffect(() => {
-    AdMobInterstitial.setAdUnitID(AdMobUnit.Interstitial.Premium);
-    AdMobInterstitial.removeAllListeners();
-
-    AdMobInterstitial.addEventListener('adClosed', () => {
-      console.log('AdMobInterstitial CLOSED!');
-      Toaster.ShowToast(Translator('Toast.DeveloperIsHappy'), 'LONG');
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const { isPremiumTime } = useContext(ProfileContext);
 
   return (
     <AdButtonContainer>
@@ -35,12 +21,6 @@ const AdIconsBar = () => {
               <Icon name="gift" color="#fff" size={25} />
             </TouchableOpacity>
           </AdButtonBlock>
-
-          {/* <AdButtonBlock alignToRight>
-            <TouchableOpacity onPress={() => {}}>
-              <Icon name="utensils" color="#fff" size={25} />
-            </TouchableOpacity>
-          </AdButtonBlock> */}
         </>
       )}
     </AdButtonContainer>
